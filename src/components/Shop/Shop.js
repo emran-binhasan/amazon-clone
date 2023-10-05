745b2547a398324b1fe5be0a58ff46eb94e4fd2e
Emran Bin Hasan <emranbinhasan.works@gmail.com>
2023-10-05 15:38:42 +0600

    cant import fakedata folder

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FakeData from '../../FakeData'
+const Shop = () => {
+    console.log(FakeData)
+    console.log()
+    return (
+        <div>
+            <h1>This is Shop</h1>
+        </div>
+       
+    );
+};
+
+export default Shop;
